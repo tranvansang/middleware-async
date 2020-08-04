fc5dbb77fcf742a1e6a0f84b9444ef2e7606fc73
Tran Sang <info@transang.me>
2020-08-05 02:43:15 +0900

    remove tslint

--- index.ts
+++ index.ts
@@ -64,7 +64,7 @@ export const middlewareToPromise = (middleware: RequestHandler) => (req: Request
 				if (err) reject(err)
 				else resolve()
 			})
-		} catch (e){} // tslint:disable-line:no-empty
+		} catch (e){}
 	}
 )
 
