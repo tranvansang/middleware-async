1009277073e6cba9972cc5c7e403b73d6926801c
Sang Tran <t@sang.jp>
2023-08-11 20:43:40 +0900

    module interop

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 rm -rf index.d.cts index.cjs index.mjs index.d.mts coverage \
-&& yarn tsc -d --skipLibCheck --target esnext --declaration --moduleResolution node --module esnext --outDir . --inlineSourceMap index.ts \
+&& yarn tsc -d --skipLibCheck --target esnext --esModuleInterop --declaration --moduleResolution node --module esnext --outDir . --inlineSourceMap index.ts \
 && mv index.js index.mjs \
 && mv index.d.ts index.d.mts \
-&& yarn tsc -d --skipLibCheck --target esnext --declaration --moduleResolution node --module commonjs --outDir . --inlineSourceMap index.ts \
+&& yarn tsc -d --skipLibCheck --target esnext --esModuleInterop --declaration --moduleResolution node --module commonjs --outDir . --inlineSourceMap index.ts \
 && mv index.d.ts index.d.cts \
 && mv index.js index.cjs
